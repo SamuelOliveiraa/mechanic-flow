d8960ea936d2a9fa298f52fbfbf4ffa8852c2220
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2026-05-11 11:04:38 -0300

    Initial commit

---
+++ src/main/java/dev/samuel/mechanicflow/MechanicflowApplication.java
@@ -0,0 +1,13 @@
+package dev.samuel.mechanicflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechanicflowApplication.class, args);
+	}
+
+}
